5c5ba9513f0517c030cd519d30703384b429f8cb
RobertB <szabolcsi.robert@gmail.com>
2020-10-13 14:35:38 +0200

    min method

--- src/org/progmatic/edu/Main.java
+++ src/org/progmatic/edu/Main.java
@@ -10,5 +10,16 @@ public class Main {
             numbers[i] = (int)(Math.random()*100+1);
         }
         System.out.println(Arrays.toString(numbers));
+        System.out.println(minArr(numbers));
+    }
+
+    public static int minArr(int[] arr){
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min){
+                min = arr[i];
+            }
+        }
+        return min;
     }
 }
