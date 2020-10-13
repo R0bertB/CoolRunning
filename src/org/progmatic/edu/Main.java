7658f1b4fceaf326b73979647e924edc65be3d75
RobertB <szabolcsi.robert@gmail.com>
2020-10-13 14:32:03 +0200

    random 20d100

--- src/org/progmatic/edu/Main.java
+++ src/org/progmatic/edu/Main.java
@@ -1,8 +1,14 @@
 package org.progmatic.edu;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] numbers = new int[20];
+        for (int i = 0; i < 20; i++) {
+            numbers[i] = (int)(Math.random()*100+1);
+        }
+        System.out.println(Arrays.toString(numbers));
     }
 }
